08b36c95cf890efda5a6f5f96689ed2703cb3d05
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-10 21:58:08 +0300

    Create initial gem

---
+++ lib/zaid2python.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "zaid2python/version"
+
+module Zaid2Python
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/zaid2python/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Zaid2Python
+  VERSION = "0.0.0"
+end
